930c93395059bab783b0b82311be3f680b05450e
froisfernanda <127303960+froisfernanda@users.noreply.github.com>
2024-09-24 09:12:11 -0300

    Add files via upload

---
+++ FILME---gente-grande-main/untitled/src/OutroLoop.java
@@ -0,0 +1,24 @@
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.Scanner;
+
+public class OutroLoop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner (System.in);
+        double mediaAvaliaçao = 0;
+        double nota = 0;
+        int totalDeNotas = 0;
+
+        while (nota!= -1) {
+            System.out.println("Diga sua avaliaçao para o filme ou -1 para encerrar");
+            nota = leitura.nextDouble();
+
+            if (nota == -1) {
+                mediaAvaliaçao += nota;
+                totalDeNotas++;
+            }
+        }
+
+        System.out.println("Media de avaliaçoes " + mediaAvaliaçao/totalDeNotas);
+    }
+}
